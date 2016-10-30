4b495019f472609ca8b7d1e6907913a047ef5c7b
Andrew Ryan <aryan17@mail.csuchico.edu>
2016-10-29 20:56:46 -0700

    basic validation complete

--- p2.js
+++ p2.js
@@ -1,1 +1,21 @@
-function ()
+function validate()
+{
+	var userEntered = document.getElementById("usr").value;
+	console.log(userEntered);
+	var userLength = userEntered.length;
+
+	//error checking courtesy of x15-InstantValidation lab
+	if(userLength < 10)
+	{
+		document.getElementById("usrError").innerHTML="Incorrect.";
+	    document.getElementById("usrGroup").classList.add("has-error");
+	    document.getElementById("usrGroup").classList.remove("has-success");
+	    console.log("inside conditional check");
+	}
+	else
+	{
+		document.getElementById("usrError").innerHTML="Congratulations you got it!";
+		document.getElementById("usrGroup").classList.add("has-success");
+		document.getElementById("usrGroup").classList.remove("has-error");
+	}
+}
