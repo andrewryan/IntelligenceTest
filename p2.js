883315e57c32a87e5df8d8960012e5199e8bc630
Andrew Ryan <aryan17@mail.csuchico.edu>
2016-10-30 18:54:43 -0700

    only results page left to do

--- p2.js
+++ p2.js
@@ -1,16 +1,18 @@
-function validate()
+function validateQ1()
 {
 	var userEntered = document.getElementById("usr").value;
-	console.log(userEntered);
-	var userLength = userEntered.length;
-
 	//error checking courtesy of x15-InstantValidation lab
-	if(userLength < 10)
+	if(isNaN(userEntered) == true)
 	{
-		document.getElementById("usrError").innerHTML="Incorrect.";
+		document.getElementById("usrError").innerHTML="The answer must only contain numbers, please try again.";
 	    document.getElementById("usrGroup").classList.add("has-error");
 	    document.getElementById("usrGroup").classList.remove("has-success");
-	    console.log("inside conditional check");
+	}
+	else if(userEntered != 18)
+	{
+		document.getElementById("usrError").innerHTML="Incorrect, keep trying.";
+	    document.getElementById("usrGroup").classList.add("has-error");
+	    document.getElementById("usrGroup").classList.remove("has-success");
 	}
 	else
 	{
@@ -18,4 +20,66 @@ function validate()
 		document.getElementById("usrGroup").classList.add("has-success");
 		document.getElementById("usrGroup").classList.remove("has-error");
 	}
+}
+function validateQ2()
+{
+	var userEntered = document.getElementById("usr").value;
+	//error checking courtesy of x15-InstantValidation lab
+	if(isNaN(userEntered) == true)
+	{
+		document.getElementById("usrError").innerHTML="The answer must only contain numbers, please try again.";
+	    document.getElementById("usrGroup").classList.add("has-error");
+	    document.getElementById("usrGroup").classList.remove("has-success");
+	}
+	else if(userEntered != 26)
+	{
+		document.getElementById("usrError").innerHTML="Incorrect, keep trying.";
+	    document.getElementById("usrGroup").classList.add("has-error");
+	    document.getElementById("usrGroup").classList.remove("has-success");
+	}
+	else
+	{
+		document.getElementById("usrError").innerHTML="Congratulations you got it!";
+		document.getElementById("usrGroup").classList.add("has-success");
+		document.getElementById("usrGroup").classList.remove("has-error");
+	}
+}
+function validateQ3()
+{
+	var userEntered = document.getElementById("usr").value;
+	//error checking courtesy of x15-InstantValidation lab
+	if(isNaN(userEntered) == true)
+	{
+		document.getElementById("usrError").innerHTML="The answer must only contain numbers, please try again.";
+	    document.getElementById("usrGroup").classList.add("has-error");
+	    document.getElementById("usrGroup").classList.remove("has-success");
+	}
+	else if(userEntered != 3)
+	{
+		document.getElementById("usrError").innerHTML="Incorrect, keep trying.";
+	    document.getElementById("usrGroup").classList.add("has-error");
+	    document.getElementById("usrGroup").classList.remove("has-success");
+	}
+	else
+	{
+		document.getElementById("usrError").innerHTML="Congratulations you got it!";
+		document.getElementById("usrGroup").classList.add("has-success");
+		document.getElementById("usrGroup").classList.remove("has-error");
+	}
+}
+function navPage2()
+{
+    location.assign("file:///Users/staceyalves/Desktop/CINS%20110/p02-IntelligenceTest/q2.html?");
+}
+function navPage3()
+{
+    location.assign("file:///Users/staceyalves/Desktop/CINS%20110/p02-IntelligenceTest/q3.html");
+}
+function navResults()
+{
+    location.assign("file:///Users/staceyalves/Desktop/CINS%20110/p02-IntelligenceTest/results.html");
+}
+function navRestart()
+{
+    location.assign("file:///Users/staceyalves/Desktop/CINS%20110/p02-IntelligenceTest/index.html?");
 }
